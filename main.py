91594e31a203e5104ea996afb664d52121e22c42
Barrizzz <barripratama.bp@gmail.com>
2024-10-26 21:20:52 +0700

    first commit

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pygame
 pygame.init()
 
-screenWidth = 800
-screenHeight = 600
-screen = pygame.display.set_mode((screenWidth, screenHeight))
+screen = pygame.display.set_mode((1000, 600))
+clock = pygame.time.Clock()
+FPS = 50
 
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+def game_menu():
+    image_bg = pygame.image.load("background.jpg")
+    image_bg = pygame.transform.scale(image_bg, (1000, 600))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 
+        screen.fill((0, 0, 0))
+        screen.blit(image_bg, (0, 0))
+        pygame.display.update()
+        clock.tick(FPS)
 
+game_menu()
 pygame.QUIT
