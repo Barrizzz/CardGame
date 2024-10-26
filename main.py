367ccff31db36ab2b01001909decde472b345bfd
Barrizzz <barripratama.bp@gmail.com>
2024-10-26 13:51:42 +0700

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pygame
 pygame.init()
 
+screenWidth = 800
+screenHeight = 600
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.QUIT
