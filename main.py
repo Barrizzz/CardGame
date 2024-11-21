507af9c5bd5bea145a2206b32a4c71ffb45820e5
Barrizzz <barripratama.bp@gmail.com>
2024-11-21 16:55:32 +0700

    s

--- main.py
+++ main.py
@@ -346,7 +346,7 @@ def start_main_game():
                 game_menu()
 
         pygame.display.flip()  # Update the display
-        clock.tick(FPS + 20)
+        clock.tick(FPS + 20) # make the fps bigger
 
 def options():
     while True:
